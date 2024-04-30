f5345cedae7460ad254a11d78ef15fa449258d00
Jamie <qwer.kear@gmail.com>
2024-04-30 12:30:44 -0400

    barebones login

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const FormDataModel = require ('./models/FormData');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/mongo_local');
+
+app.post('/register', (req, res)=>{
+
+    const {email, password} = req.body;
+    FormDataModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            res.json("Already registered")
+        }
+        else{
+            FormDataModel.create(req.body)
+            .then(log_reg_form => res.json(log_reg_form))
+            .catch(err => res.json(err))
+        }
+    })
+    
+})
+
+app.post('/login', (req, res)=>{
+    const {email, password} = req.body;
+    FormDataModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password) {
+                res.json("Success");
+            }
+            else{
+                res.json("Wrong password");
+            }
+        }
+        else{
+            res.json("No records found! ");
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log("Server listining on http://127.0.0.1:3001");
+
+});
